30bf874c1444903931be4be1290195f035b7ff4c
Priyanka <priyanka3007@gmail.com>
2020-05-20 16:52:06 -0700

    started localization testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LocalizedStrings from 'react-localization';
 
 function App() {
+  let strings = new LocalizedStrings({
+ en:{
+   how:"How do you want your egg today?",
+   boiledEgg:"Boiled egg",
+   softBoiledEgg:"Soft-boiled egg",
+   choice:"How to choose the egg"
+ },
+ it: {
+   how:"Come vuoi il tuo uovo oggi?",
+   boiledEgg:"Uovo sodo",
+   softBoiledEgg:"Uovo alla coque",
+   choice:"Come scegliere l'uovo"
+ }
+
+
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{strings.how}</p>
     </div>
   );
 }
