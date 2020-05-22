dd3edc18bbaa16d50792972e93ad99441d67307c
Priyanka <priyanka3007@gmail.com>
2020-05-22 16:35:32 -0700

    langauge localization

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 import LocalizedStrings from 'react-localization';
-
-function App() {
-  let strings = new LocalizedStrings({
- en:{
-   how:"How do you want your egg today?",
-   boiledEgg:"Boiled egg",
-   softBoiledEgg:"Soft-boiled egg",
-   choice:"How to choose the egg"
- },
- it: {
-   how:"Come vuoi il tuo uovo oggi?",
-   boiledEgg:"Uovo sodo",
-   softBoiledEgg:"Uovo alla coque",
-   choice:"Come scegliere l'uovo"
- }
+// import { data } from './langaugeData';
 
 
-  });
+let strings = new LocalizedStrings({
+  en:{
+    how:"How do you want your egg today?",
+    boiledEgg:"Boiled egg",
+    softBoiledEgg:"Soft-boiled egg",
+    choice:"How to choose the egg"
+  },
+  it: {
+    how:"Come vuoi il tuo uovo oggi?",
+    boiledEgg:"Uovo sodo",
+    softBoiledEgg:"Uovo alla coque",
+    choice:"Come scegliere l'uovo"
+  }
+ });
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: 'en'
+    }
 
+    this.handleLanguageChange = this.handleLanguageChange.bind(this);
+  }
 
-  return (
-    <div className="App">
-      <p>{strings.how}</p>
-    </div>
-  );
+  handleLanguageChange(e) {
+    e.preventDefault();
+    let lang = e.target.value;
+    console.log(lang)
+    this.setState(prevState => ({
+      language: lang
+    }))
+  }
+
+  render() {
+    strings.setLanguage(this.state.language);
+    return (
+      <div>
+        Change Language: <select onChange={this.handleLanguageChange}>
+          <option value="en">En- English</option>
+          <option value="it">It- Italian</option>
+        </select>
+        <br /><br />
+        <p>{strings.how}</p>
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/langaugeData.js
@@ -0,0 +1,15 @@
+const data = {
+  en:{
+    how:"How do you want your egg today?",
+    boiledEgg:"Boiled egg",
+    softBoiledEgg:"Soft-boiled egg",
+    choice:"How to choose the egg"
+  },
+  it: {
+    how:"Come vuoi il tuo uovo oggi?",
+    boiledEgg:"Uovo sodo",
+    softBoiledEgg:"Uovo alla coque",
+    choice:"Come scegliere l'uovo"
+  }
+}
+export {data};
